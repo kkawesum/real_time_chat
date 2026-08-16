01f18d49d49054027de4b1631827b726c0c08ad2
kk <you@example.com>
2026-08-16 17:08:21 +0530

    auth views added

--- backend/config/accounts/serializers.py
+++ backend/config/accounts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import User
 
@@ -21,3 +22,53 @@ class UserSerializer(serializers.ModelSerializer):
             "last_seen",
             "is_online",
         )
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+
+        fields = [
+            "phone_number",
+            "first_name",
+            "last_name",
+            "password",
+        ]
+
+    def create(self, validated_data):
+
+        password = validated_data.pop("password")
+
+        user = User.objects.create_user(
+            password=password,
+            **validated_data
+        )
+
+        return user
+
+class LoginSerializer(serializers.Serializer):
+
+    phone_number = serializers.CharField()
+
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+
+        request = self.context.get("request")
+
+        user = authenticate(
+            request=request,
+            phone_number=attrs["phone_number"],
+            password=attrs["password"],
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        attrs["user"] = user
+
+        return attrs
+
